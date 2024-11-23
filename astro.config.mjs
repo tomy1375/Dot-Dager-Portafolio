9ca30d872d72e9a727cf8d960c930a4fa9932d27
tomy1375 <tomy_ramos1991@yahoo.com.ar>
2024-11-22 21:42:41 -0300

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
   integrations: [tailwind(), react()],
-  
   output: 'server',
-  adapter: vercel(),
+  adapter: netlify(),
 });
